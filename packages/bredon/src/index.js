7adf91fe390edcd3605fa58f693952be5562cfe5
rofrischmann <robin@rofrischmann.de>
2017-06-17 21:07:35 +0200

    fix naming case

--- packages/bredon/src/index.js
+++ packages/bredon/src/index.js
@@ -10,23 +10,24 @@ type TransformOptions = {
   generators?: Object
 }
 
-function parse(input: string): AST {
+export function parse(input: string): AST {
   const parser = new Parser()
   return parser.parse(input)
 }
 
-function generate(ast: AST, generators: Object = {}): string {
+export function generate(ast: AST, generators: Object = {}): string {
   const generator = new Generator(generators)
   return generator.generate(ast)
 }
 
-function traverse(ast: AST, visitors: Object = {}): void {
+export function traverse(ast: AST, visitors: Object = {}): void {
   const traverser = new Traverser(visitors)
   return traverser.traverse(ast)
 }
 
-function transform(input: string): string {
-  return generate(traverse(parse(input)))
+export function transform(
+  input: string,
+  options: TransformOptions = {}
+): string {
+  return generate(traverse(parse(input), options.visitors), options.generators)
 }
-
-export { parse, generate, traverse, transform }
